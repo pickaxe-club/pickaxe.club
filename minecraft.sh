d8657a3a088eeaa94f95b624dc2d14246c3aece5
Nick Quaranto <nick@quaran.to>
2015-01-12 09:46:54 -0500

    Weekend 6 final

--- minecraft.sh
+++ minecraft.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-exec java -Xms1G -Xmx4G -jar minecraft_server.jar nogui
+#exec java -Xms4G -Xmx16G -XX:ParallelGCThreads=8 -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalPacing -XX:+AggressiveOpts
+exec java -Xms4G -Xmx16G -XX:+UseFastAccessorMethods -XX:+AggressiveOpts -XX:+DisableExplicitGC -XX:+UseAdaptiveGCBoundary -XX:MaxGCPauseMillis=500 -XX:SurvivorRatio=16 -XX:+UseParallelGC -XX:UseSSE=3 -XX:ParallelGCThreads=8 -jar minecraft_server.jar nogui
