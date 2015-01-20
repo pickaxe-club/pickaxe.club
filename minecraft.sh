88f089706111ebceb3725495ff322f884c00640c
Nick Quaranto <nick@basecamp.com>
2015-01-20 00:46:04 -0500

    Go back for testing

--- minecraft.sh
+++ minecraft.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-#exec java -Xms4G -Xmx16G -XX:ParallelGCThreads=8 -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalPacing -XX:+AggressiveOpts
-exec java -Xms4G -Xmx16G -XX:+UseFastAccessorMethods -XX:+AggressiveOpts -XX:+DisableExplicitGC -XX:+UseAdaptiveGCBoundary -XX:MaxGCPauseMillis=500 -XX:SurvivorRatio=16 -XX:+UseParallelGC -XX:UseSSE=3 -XX:ParallelGCThreads=8 -jar minecraft_server.jar nogui
+# was: 4G, 16G
+exec java -Xms512M -Xmx1G -XX:+UseFastAccessorMethods -XX:+AggressiveOpts -XX:+DisableExplicitGC -XX:+UseAdaptiveGCBoundary -XX:MaxGCPauseMillis=500 -XX:SurvivorRatio=16 -XX:+UseParallelGC -XX:UseSSE=3 -XX:ParallelGCThreads=8 -jar minecraft_server.jar nogui
