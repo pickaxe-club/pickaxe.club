7fe758ba9f1fe5de22486bf13927fa8a9396fb9a
Nick Quaranto <nick@quaran.to>
2015-01-24 21:15:48 +0000

    Post crash backup

--- minecraft.sh
+++ minecraft.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-# was: 4G, 16G
-exec java -Xms512M -Xmx1G -XX:+UseFastAccessorMethods -XX:+AggressiveOpts -XX:+DisableExplicitGC -XX:+UseAdaptiveGCBoundary -XX:MaxGCPauseMillis=500 -XX:SurvivorRatio=16 -XX:+UseParallelGC -XX:UseSSE=3 -XX:ParallelGCThreads=8 -jar minecraft_server.jar nogui
+exec java -Xms4G -Xmx16G -XX:+UseFastAccessorMethods -XX:+AggressiveOpts -XX:+DisableExplicitGC -XX:+UseAdaptiveGCBoundary -XX:MaxGCPauseMillis=500 -XX:SurvivorRatio=16 -XX:+UseParallelGC -XX:UseSSE=3 -XX:ParallelGCThreads=8 -jar minecraft_server.jar nogui
