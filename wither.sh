65db6deca565aa3190cc4a246420c7dfe36a721d
Nick Quaranto <nick@quaran.to>
2016-01-17 13:08:31 -0500

    Escape wither input please

--- wither.sh
+++ wither.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+set -f
+GLOBIGNORE="*"
 
-tail -F /home/minecraft/pickaxe.club/logs/latest.log | grep --line-buffered ": <" | while read x ; do echo -ne $x | curl -X POST -d @- https://wither.herokuapp.com/minecraft/hook ; done
+tail -F /home/minecraft/pickaxe.club/logs/latest.log | grep --line-buffered ": <" | while read x ; do echo -ne "$x" | curl -X POST -d @- https://wither.herokuapp.com/minecraft/hook ; done
