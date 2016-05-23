e9b486266bf37d6ab9918bcdd5b3e77456138d09
Nick Quaranto <nick@quaran.to>
2016-05-23 07:29:07 -0400

    Bring back gitignores

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Backing up week$WEEK..."
-git add -A .; git commit -m "Weekend final"; git push
-tar cf - . | pigz > "week$WEEK.tar.gz"
+git add -A .; git commit -m "Weekend $WEEK final"; git push origin master
+tar -cvzf "week$WEEK.tar.gz" .
 aws s3 cp "week$WEEK.tar.gz" s3://archive.pickaxe.club/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --region=us-east-1     
