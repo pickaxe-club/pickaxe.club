25c0a818a48d0f5223e736486197b23654e91b14
Paul Morganthall <farbot@gmail.com>
2021-02-22 17:47:11 +0000

    Weekend 323 final

--- backup.sh
+++ backup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "Backing up week$WEEK..."
-git add -A .; git commit -m "Weekend $WEEK final"; git push origin master
+git add -A .; git commit -m "Weekend $WEEK final" 
+# git push origin master -- no key available for pushing at this time. UncleAdam
 tar --use-compress-program=pigz --exclude="*.tar.gz" -cvf ../week$WEEK.tar.gz .
 aws s3 cp "../week$WEEK.tar.gz" s3://archive.pickaxe.club/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --region=us-east-1     
